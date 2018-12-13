443cab76f15f54f20fe8ce1efae65f9cde67b5b6
Raphael Merx <raphael.merx@gmail.com>
2018-12-13 12:10:13 +0900

    Add markdown support

--- gettext.js
+++ gettext.js
@@ -3,7 +3,7 @@
 (function (global) {
     var catalog = { 'en': {} };
 
-    global.gettext = function (messageId, variables) {
+    global.gettext = function (messageId, variables, useMarkdown) {
         var replaceRegex = void 0;
         var translation = catalog[global.languageCode][messageId];
         translation = typeof translation === 'undefined' ? messageId : translation;
@@ -16,7 +16,36 @@
             translation = translation.replace(replaceRegex, value);
         });
 
-        return translation;
+        return useMarkdown ? global.markdownText(translation) : translation;
+    };
+
+    /** Convert any markdown in the sourceText, stripping off any bracketing HTML tags */
+    global.markdownText = function (sourceText) {
+        var conversion = mdConverter.makeHtml(sourceText).trim();
+
+        // Get the HTML tag (the last one)
+        var tagRegex = /<\/([a-z]*)>$/;
+        var tagMatch = conversion.match(tagRegex);
+        var tag;
+        var tagStrip;
+        var deTagged;
+
+        if (tagMatch.length === 0 || tagMatch[1].length === 0) {
+            // this should never occur, but just in case
+            return conversion;
+        }
+
+        // Construct the tag stripping regex
+        // Note: this does not support '>' within the opening tag's attributes
+        tag = tagMatch[1];
+        tagStrip = new RegExp('^<' + tag + '.*?>((?:\n|.)*)</' + tag + '>$');
+        deTagged = conversion.match(tagStrip);
+        if (deTagged.length === 0 || deTagged[1].length === 0) {
+            // again, this should never occur, but just in case
+            return conversion;
+        }
+
+        return deTagged[1];
     };
 
     global.setLanguage = function (newLanguageCode) {
@@ -40,5 +69,5 @@ extendGettextCatalog('tet', {
     'Cancel': 'Kansela',
     'Save': 'Rai',
     'Edit': 'Hadia',
-    'Please confirm': 'Favor konfirma',
+    'Please confirm': 'Favor konfirma'
 });
